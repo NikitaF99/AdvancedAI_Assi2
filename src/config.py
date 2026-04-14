7d4e23b41418e5a7c3b6f64f96e6a799b236b563
NikitaF99 <nikitafernando1999@gmail.com>
2026-04-14 19:41:47 +0930

    feat: necessary setup files added

---
+++ src/config.py
@@ -0,0 +1,51 @@
+
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+import torch
+from pathlib import Path
+
+
+MODEL_IDS = [
+    "meta-llama/Meta-Llama-3-8B-Instruct", "meta-llama/Llama-2-7b-chat-hf", "meta-llama/Meta-Llama-3.1-8B-Instruct",
+    "google/gemma-7b-it", "google/gemma-2-2b-it", "google/gemma-2-9b-it",
+    "mistralai/Mistral-7B-Instruct-v0.3", "mistralai/Mistral-Nemo-Instruct-2407", "mistralai/Pixtral-12B-2409",
+    "Qwen/Qwen2.5-7B-Instruct", "Qwen/Qwen2-7B-Instruct", "Qwen/Qwen1.5-7B-Chat",
+    "deepseek-ai/deepseek-llm-7b-chat", "deepseek-ai/deepseek-coder-6.7b-instruct", "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
+]
+
+PROMPT_1 = """I want to make three personas, and the three agents. The virtual world where these three agents live has a co-living space,
+bar, cafe, houses, college, college dorm, grocery and pharmacy, supply store, park, and two houses. Can you create personas of all
+three agents for me? I want you to provide me, with their Age, Educational Qualification, Personality Traits, Devices and technologies
+they use, Work experience, Domain of work, Country, Gender with the following requirements:
+Requirements:
+• Names (mandatory): Ensure the names reflect a variety of ethnicities and faiths.
+• Gender (mandatory): Include a balanced representation of different genders.
+• Age (mandatory): Include a balanced representation of different genders.
+• Personality Traits (mandatory): Include diverse personality traits
+• Domain of Work (mandatory): Focus on diverse roles.
+• Geographical Location (mandatory): Represent various regions globally.
+• Few other mandatory requirement are education level, years of experience.
+• Character Limit (optional): Each profile must be concise, within 300 characters"""
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data"
+DATA_DIR.mkdir(exist_ok=True)
+
+PARSED_PERSONAS_JSON = DATA_DIR / "parsed_personas.json"
+PERSONAS_CSV = DATA_DIR / "personas_data.csv"
+PHISHING_JSON = DATA_DIR / "final_phishing_audit_results.json"
+ITERATION_CSV = DATA_DIR / "final_personas_with_prompt2_iteration.csv"
+SELECTED_ONLY_CSV = DATA_DIR / "final_personas_selected_only.csv"
+
+STEP1_BNB_CONFIG = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_use_double_quant=True,
+)
+
+STEP3_BNB_CONFIG = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
